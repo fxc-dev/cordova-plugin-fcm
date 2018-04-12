0b2689cd537d7aa26474a4d3bf06d0a0c7f485d0
fxc-dev <vazhenin-sv@fxclub.org>
2018-04-12 14:08:27 +0700

    Update FCMPlugin.m
    FCM + Donky improvements

--- src/ios/FCMPlugin.m
+++ src/ios/FCMPlugin.m
@@ -6,7 +6,14 @@
 #import <Cordova/CDV.h>
 #import "FCMPlugin.h"
 #import "Firebase.h"
+// FCM + Donky improvements
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@import UserNotifications;
+#endif
 
+@import FirebaseInstanceID;
+@import FirebaseMessaging;
+// FCM + Donky improvements
 @interface FCMPlugin () {}
 @end
 
@@ -73,7 +80,82 @@ static FCMPlugin *fcmPluginInstance;
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
+// FCM + Donky improvements
+- (void) registerForPush:(CDVInvokedUrlCommand *)command
+{
+    [self.commandDelegate runInBackground:^{
+            UIApplication *application = [UIApplication sharedApplication];
+            id<UIApplicationDelegate> appDelegate = application.delegate;
+            // Register for remote notifications. This shows a permission dialog on first run, to
+            // show the dialog at a more appropriate time move this registration accordingly.
+            if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+                // iOS 7.1 or earlier. Disable the deprecation warnings.
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+                UIRemoteNotificationType allNotificationTypes =
+                (UIRemoteNotificationTypeSound |
+                 UIRemoteNotificationTypeAlert |
+                 UIRemoteNotificationTypeBadge);
+                [application registerForRemoteNotificationTypes:allNotificationTypes];
+    #pragma clang diagnostic pop
+            } else {
+                // iOS 8 or later
+                // [START register_for_notifications]
+                if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+                    UIUserNotificationType allNotificationTypes =
+                    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+                    UIUserNotificationSettings *settings =
+                    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+                    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+                } else {
+                    // iOS 10 or later
+    #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+                    UNAuthorizationOptions authOptions =
+                    UNAuthorizationOptionAlert
+                    | UNAuthorizationOptionSound
+                    | UNAuthorizationOptionBadge;
+                    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                    }];
+                    
+                    // For iOS 10 display notification (sent via APNS)
+                    [UNUserNotificationCenter currentNotificationCenter].delegate = appDelegate;
+                    // For iOS 10 data message (sent via FCM)
+                    [FIRMessaging messaging].remoteMessageDelegate = appDelegate;
+    #endif
+                }
+                
+                [application registerForRemoteNotifications];
+                // [END register_for_notifications]
+                NSLog(@"FCM registered for push");
 
+            
+        }
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+
+- (void)hasPermission:(CDVInvokedUrlCommand *)command
+{
+    BOOL enabled = NO;
+    UIApplication *application = [UIApplication sharedApplication];
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        enabled = application.currentUserNotificationSettings.types != UIUserNotificationTypeNone;
+    } else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        enabled = application.enabledRemoteNotificationTypes != UIRemoteNotificationTypeNone;
+#pragma GCC diagnostic pop
+    }
+    
+    NSMutableDictionary* message = [NSMutableDictionary dictionaryWithCapacity:1];
+    [message setObject:[NSNumber numberWithBool:enabled] forKey:@"isEnabled"];
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+}
+// FCM + Donky improvements
 - (void) registerNotification:(CDVInvokedUrlCommand *)command
 {
     NSLog(@"view registered for notifications");
